2e341ab054146c4de81e22413dc50cbe278d88b5
Sakib <bksumon51@gmail.com>
2023-05-02 23:31:50 +0600

    created recipes data

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const app = express()
 const port = 3000
 const cors = require('cors')
 const chefDetails = require('./data/details.json')
+const recipes = require('./data/recipes.json')
 app.use(cors())
 app.get('/', (req, res) => {
     res.send('hello world')
 })
 app.get('/chef-details', (req, res) => {
     res.send(chefDetails)
+})
+app.get('/recipes', (req, res) => {
+    res.send(recipes)
 })
 app.listen(port)
